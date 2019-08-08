feafb117bbf7e2db3d4590e8e9691a78df4391f7
woodney <woodneyguerrier@gmail.com>
2019-08-08 10:01:28 -0500

    latest update

---
+++ project2/src/main/java/com/revature/project2/UsersRepository.java
@@ -0,0 +1,7 @@
+package com.revature.project2;
+import org.springframework.data.repository.CrudRepository;
+import com.revature.project2.Users;
+
+public interface UsersRepository extends CrudRepository<Users, Integer> {
+
+}
