5cea5c05498672b73452f134112b5bfc601a5182
woodney <woodneyguerrier@gmail.com>
2019-08-10 10:59:44 -0500

    cart added

--- project2/src/main/java/com/revature/project2/Repositories/UsersRepository.java
+++
@@ -1,7 +0,0 @@
-package com.revature.project2.Repositories;
-import org.springframework.data.repository.CrudRepository;
-import com.revature.project2.Entities.Users;
-
-
-public interface UsersRepository extends CrudRepository<Users, Integer> {
-}
