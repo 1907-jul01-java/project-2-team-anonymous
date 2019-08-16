bb9a7d69a659d60a2117b2706097ce86c4e6e09c
woodney <woodneyguerrier@gmail.com>
2019-08-16 10:43:39 -0500

    blah

--- project2/src/main/java/com/revature/project2/repos/ProductsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.revature.project2.repos;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-
-import com.revature.project2.models.Products;
-
-public interface ProductsRepository extends JpaRepository<Products, Integer> {
-
-}
